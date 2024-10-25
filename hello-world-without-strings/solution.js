124e0b24b77e51f96affd1735046ab60f327c6d3
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2024-10-25 14:37:08 -0300

    Add files via upload

---
+++ hello-world-without-strings/solution.js
@@ -0,0 +1,17 @@
+const helloWorld = () => {
+  return String.fromCharCode(
+    72,
+    101,
+    108,
+    108,
+    111,
+    44,
+    32,
+    87,
+    111,
+    114,
+    108,
+    100,
+    33
+  );
+};
