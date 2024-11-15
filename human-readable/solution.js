dd51dd7054441fd4d2aff8bdd8e01fbb1a57c6b1
Luiz Felipe <luizfelipedasilva678@gmail.com>
2024-11-15 12:54:05 -0300

    Refactoring

--- human-readable/solution.js
+++ human-readable/solution.js
@@ -1,15 +1,11 @@
 function humanReadable(s) {
-  let hours;
-  let minutes;
-  let seconds;
-
-  hours = Math.floor(s / 3600);
-  minutes = Math.floor((s - hours * 3600) / 60);
-  seconds = s - hours * 3600 - minutes * 60;
+  const hours = Math.floor(s / 3600);
+  const minutes = Math.floor((s - hours * 3600) / 60);
+  const seconds = s - hours * 3600 - minutes * 60;
 
   return `${hours.toString().padStart(2, "0")}:${minutes
     .toString()
     .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
-console.log(humanReadable(86400));
+console.log(humanReadable(8));
