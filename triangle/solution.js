c71bd7b861910ce870673314c8253c4387ecf45e
Luiz Felipe <luizfelipedasilva678@gmail.com>
2025-03-08 00:41:47 -0300

    Adjustment

--- triangle/solution.js
+++ triangle/solution.js
@@ -7,7 +7,7 @@ function minimumTotal(triangle, row = 0, col = 0, memo = new Map()) {
   const key = `${row}-${col}`;
   if (memo.has(key)) return memo.get(key);
   const currentValue = triangle[row][col];
-  if (row === triangle.length - 1) return triangle[row][col];
+  if (row === triangle.length - 1) return currentValue;
 
   const path1 = minimumTotal(triangle, row + 1, col, memo);
   const path2 = minimumTotal(triangle, row + 1, col + 1, memo);
